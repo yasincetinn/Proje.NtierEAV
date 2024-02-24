1b69f22d774b75975ed36dfb3ace753eda17bcda
cihan dilibal <dilibalcihan@gmail.com>
2024-02-24 15:55:29 +0300

    dal

---
+++ Proje.CONF/Options/OrderConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje.CONF.Options
+{
+    internal class OrderConfiguration
+    {
+    }
+}
